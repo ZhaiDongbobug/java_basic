c20218307a500f8bebfa916f1396c697c29b4180
ZhaiDongboBug <18747867831@163.com>
2020-02-22 18:08:22 +0800

    使能控制台输入输出

--- src/basic1/Account.java
+++ src/basic1/Account.java
@@ -10,27 +10,60 @@ public class Account {
 		return this.balance; 
 	}
 	//存钱
-	public void deposit(int money) {
-		balance += money;
+	public boolean deposit(int money) {
+		if(money > 0) {
+			balance += money;
+		}else {
+			System.out.println("输入金额有误！请重新输入：");
+			return false;
+		}
+		return true;
 	}
 	//取钱
 	public void withdraw(int money) throws OverdraftException {
 		if(deficit>(balance-money)) {
 			throw new OverdraftException("超出透支额度！！！");
+		}else {
+			balance -= money;
+			System.out.println("取钱成功！");
 		}
-		balance -= money; 
 	}
-	
+	public static void service(Account ac) throws OverdraftException {
+		System.out.println("请选择你的业务！");
+		System.out.println("1.查询当前余额");
+		System.out.println("2.存钱");
+		System.out.println("3.取钱");
+		System.out.println("请输入数字：");
+		
+		int money = 0;
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		switch(i)
+		{
+		case 1:
+			int balanceTempt = ac.getBalance();
+			System.out.println("当前余额为："+ balanceTempt);
+			break;
+		case 2:
+			System.out.println("请输入您要存储的金额");
+			money = sc.nextInt();
+			boolean dep = ac.deposit(money);
+			if(dep==true) {
+				System.out.println("存款成功！");
+			}
+			break;
+		case 3:
+			System.out.println("请输入要取出的金额：");
+			money = sc.nextInt();
+			ac.withdraw(money);
+			break;	
+		}
+	}
 	public static void main(String[] args) throws OverdraftException {
 		// TODO Auto-generated method stub
-		System.out.println("请输入数字：\n");
-		//Scanner s = new Scanner(System.in);
+		
 		Account ac = new Account();
-		System.out.println(ac.getBalance());
-		//ac.deposit(s.nextInt());
-		//System.out.println(ac.getBalance());
-		Scanner sc = new Scanner(System.in);
-		ac.withdraw(sc.nextInt());
+		service(ac);
 		
 	}
 
