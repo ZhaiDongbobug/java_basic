7f06c559399b0a4a5eb7e6e5cb9229a7899f033f
ZhaiDongboBug <18747867831@163.com>
2020-02-25 11:57:41 +0800

    IO Example1 right
    这是答案

--- src/basicExample2/fileExample.java
+++ src/basicExample2/fileExample.java
@@ -7,15 +7,29 @@ public class fileExample {
 	public static void main(String[] args) throws IOException {
 		File f = new File("C:\\windows");
 		File[] fs= f.listFiles();
-		double fd = fs[fs.length-1].length();
-		for(File fi : fs)
+		long minFileValue = Integer.MAX_VALUE;
+		long maxFileValue = 0;
+		File maxFile = null;
+		File minFile = null;
+		for(File file : fs)
 		{
-			System.out.println(fi);
-			double fu = 0;
-					
-			
+			if(file.isDirectory())
+				continue;
+			if(file.length() > maxFileValue)
+			{
+				maxFileValue = file.length();
+				maxFile = file;
+			}
+			if(file.length() != 0 && file.length() < minFileValue)
+			{
+				minFileValue = file.length();
+				minFile = file;
+			}
 		}
-		
+		System.out.printf("最大的文件是%s,其大小是%d,字节\n",
+				maxFile.getAbsoluteFile(),maxFileValue);
+		System.out.printf("最小的文件是%s,其大小是%d,字节",
+				minFile.getAbsoluteFile(),minFileValue);
 		
 	}
 }
