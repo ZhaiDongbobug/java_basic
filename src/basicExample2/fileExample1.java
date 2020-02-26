4ca269e527e66e24be468008cdfbf646392383bc
ZhaiDongboBug <18747867831@163.com>
2020-02-26 10:49:48 +0800

    递归文件夹
    1.首先要判断是文件夹还是文件
    2.如果是文件夹，要用数组表示出来，以备遍历

--- src/basicExample2/fileExample1.java
+++ src/basicExample2/fileExample1.java
@@ -1,22 +1,27 @@
 package basicExample2;
 
 import java.io.File;
-import java.nio.file.Files;
 
 public class fileExample1 {
 
-	public static void listFiles(File[] fs) {
-		for(File F : fs) {
-			if(F.isDirectory()) {
-				System.out.println("子文件夹："+F.getAbsolutePath());
-			}
+	public static void listFiles(File f) {
+		if(f.isFile()) {
+			System.out.println("文件为："+ f.getAbsolutePath());
 		}
+		if(f.isDirectory()) {
+			File[] fs= f.listFiles();
+			if(null != fs) {
+				for(File F : fs) {
+					listFiles(F);
+				}
+			}
+			
+		}
+		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		File f = new File("C:\\windows");
-		File[] fs= f.listFiles();
-		listFiles(fs);
+		listFiles(f);
 	}
-
 }
