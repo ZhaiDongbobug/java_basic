73062c73eebd1ae21412627de2733337764754f0
ZhaiDongboBug <18747867831@163.com>
2020-03-14 20:44:32 +0800

    拆分文件-1

---
+++ src/basicExample2/IoExample1.java
@@ -0,0 +1,13 @@
+package basicExample2;
+
+import java.io.File;
+
+public class IoExample1 {
+	public static void main() {
+		File f = new File("d:/xyz/abc/def/lol2.txt");
+		if(f.getTotalSpace()>100) {
+			FileInputStream fis = new FileInputStream(f);
+			fis.
+		}
+	}
+}
