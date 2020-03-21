05e861968a376774200d500b574ffcd62f943fcf
ZhaiDongboBug <18747867831@163.com>
2020-03-21 11:27:48 +0800

    IO练习-1

--- src/basicExample2/IoExample1.java
+++ src/basicExample2/IoExample1.java
@@ -1,6 +1,8 @@
 package basicExample2;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class IoExample1 {
 	public static void main() {
@@ -9,6 +11,19 @@ public class IoExample1 {
 		splitFile(srcfile,eachsize);
 	}
 	 public static void splitFile(File f,long l) {
-		 
+		try {
+			FileInputStream fis = new FileInputStream(f);
+			byte[] b = new byte[];
+			b=
+			for(int i;i<=f.length()/l;i++) {
+				Filename 
+				fis.read(b, off, l);
+				File fn =new File(null, null);
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	 }
 }
