b2a444c8dab445e44a0cbd6bc175d5152671a135
ZhaiDongboBug <18747867831@163.com>
2020-03-23 10:53:43 +0800

    IO练习-2
    1.进一步理解字节流写入：将内存中的数据通过新创建的字节流FileOutputStream写入指定的文件中
    2.此节中涉及到了异常的知识点，再复习一下：异常有三类，分别是运行时异常，可查异常，错误异常；
    运行时异常不会出现编译错误不让你通过，等你运行时给出失败提示，
    可查异常在编译之前必须处理好，要么try catch住，要么往外抛，谁调用，谁处理，比如FileNotFoundException
    如果不处理编译器就不让你通过。
    错误异常一般指内存用光了，是系统级异常

--- src/basicExample2/IoExample1.java
+++ src/basicExample2/IoExample1.java
@@ -3,6 +3,11 @@ package basicExample2;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
 
 public class IoExample1 {
 	public static void main() {
@@ -10,20 +15,39 @@ public class IoExample1 {
 		File srcfile = new File("d:/eclipse.exe");
 		splitFile(srcfile,eachsize);
 	}
-	 public static void splitFile(File f,long l) {
-		try {
+	 private static void splitFile(File f,long l) {
+		 if(0 == f.length()) {
+			 throw new RuntimeException("此文件无法分割！");
+		 }
+		 byte[] filecontent = new byte[(int) f.length()];
+		 try {
 			FileInputStream fis = new FileInputStream(f);
-			byte[] b = new byte[];
-			b=
-			for(int i;i<=f.length()/l;i++) {
-				Filename 
-				fis.read(b, off, l);
-				File fn =new File(null, null);
-			}
-			
-		} catch (FileNotFoundException e) {
+			fis.read(filecontent);
+			fis.close();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		int fileNumber;
+		
+		if(f.length() % l == 0) {
+			fileNumber = (int) (f.length()/l);
+		}else {
+			fileNumber = (int) (f.length()/l)+1;
+		}
+		for(int i = 0;i<fileNumber;i++) {
+			String fileName = "exe-"+ i;
+			byte[] eachfile = Arrays.copyOfRange(filecontent, (int) (l*i-1), (int) (l*i));
+			File file = new File(f.getParent(),fileName);
+			try {
+				FileOutputStream fos = new FileOutputStream(file);
+				fos.write(eachfile);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("");
+		}
+		
 	 }
 }
