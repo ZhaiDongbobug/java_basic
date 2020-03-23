47d4d9bc44ef0378a1e7a87c951c69b5bfe9fb77
ZhaiDongboBug <18747867831@163.com>
2020-03-23 11:28:46 +0800

    IO练习-3
    1.在C语言中不考虑其他
    long>=int>=short
    long是8个字节 int是4个字节 一个字节是8位
    当然了这个具体要看编译器，它会分配内存

--- src/basicExample2/IoExample1.java
+++ src/basicExample2/IoExample1.java
@@ -11,18 +11,18 @@ import javax.swing.plaf.synth.SynthSeparatorUI;
 
 public class IoExample1 {
 	public static void main() {
-		long eachsize = 100 * 1024;
-		File srcfile = new File("d:/eclipse.exe");
-		splitFile(srcfile,eachsize);
+		int eachSize = 100 * 1024;
+		File srcFile = new File("d:/eclipse.exe");
+		splitFile(srcFile,eachSize);
 	}
-	 private static void splitFile(File f,long l) {
-		 if(0 == f.length()) {
+	 private static void splitFile(File srcFile,int eachSize) {
+		 if(0 == srcFile.length()) {
 			 throw new RuntimeException("此文件无法分割！");
 		 }
-		 byte[] filecontent = new byte[(int) f.length()];
+		 byte[] fileContent = new byte[(int) srcFile.length()];
 		 try {
-			FileInputStream fis = new FileInputStream(f);
-			fis.read(filecontent);
+			FileInputStream fis = new FileInputStream(srcFile);
+			fis.read(fileContent);
 			fis.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -30,23 +30,33 @@ public class IoExample1 {
 		}
 		int fileNumber;
 		
-		if(f.length() % l == 0) {
-			fileNumber = (int) (f.length()/l);
+		if(srcFile.length() % eachSize == 0) {
+			fileNumber = (int) (fileContent.length/eachSize);
 		}else {
-			fileNumber = (int) (f.length()/l)+1;
+			fileNumber = (int) (fileContent.length/eachSize)+1;
 		}
 		for(int i = 0;i<fileNumber;i++) {
-			String fileName = "exe-"+ i;
-			byte[] eachfile = Arrays.copyOfRange(filecontent, (int) (l*i-1), (int) (l*i));
-			File file = new File(f.getParent(),fileName);
+			String eachFileName = srcFile.getName() + "-" + i;
+			File eachFile = new File(srcFile.getParent(),eachFileName);
+			byte[] eachContent;
+			
+			if(i != fileNumber - 1) {
+				eachContent = Arrays.
+						copyOfRange(fileContent, eachSize*i, eachSize*(i+1));
+			}else {
+				eachContent = Arrays.
+						copyOfRange(fileContent, eachSize*i, fileContent.length);
+			}
 			try {
-				FileOutputStream fos = new FileOutputStream(file);
-				fos.write(eachfile);
+				FileOutputStream fos = new FileOutputStream(eachFile);
+				fos.write(eachContent);
+				fos.close();
+				System.out.printf("输出子文件%s，其大小是 %d字节%n", 
+						eachFile.getAbsoluteFile(), eachFile.length());
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			System.out.println("");
 		}
 		
 	 }
