86079e03b123755fb47634f03275a248301fadc5
ZhaiDongboBug <18747867831@163.com>
2020-03-25 21:09:26 +0800

    将分开的文件合并-2
    1.这里要用到File对象的getParentFile方法，返回此抽象路径名的父目录的抽象路径名，
    如果此路径名不指定父目录，则返回 null
    。
      //首先创建一个File类对象，指定一个文件路径
      File f= new File("D:\\A\\BC\\2.txt");
    
    //获取文件的父目录
            File pf = f.getParentFile();
      //  并判断是否存在
    
    if(!pf.exists()){
            //若不存在，则创建父目录
                pf.mkdirs();
    
    }
            //到这里父目录已经创建好了，可以创建文件了
            f.createNewFile();
    ————————————————
    版权声明：本文为CSDN博主「Mornar_」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
    原文链接：https://blog.csdn.net/u014570939/article/details/85487227
    2.

--- src/basicExample2/IoExample2.java
+++ src/basicExample2/IoExample2.java
@@ -8,26 +8,30 @@ import java.io.IOException;
 public class IoExample2 {
 
 	public static void main(String[] args) {
-		File srcFile = new File("d:/eclipse.exe");
-		int i=0;
-		while(i<5) {
-            String eachFileName = srcFile.getName() + "-" + i;
-            i++;
-            File eachFile = new File(srcFile.getParent(), eachFileName);
-            byte[] fileContent = new byte[(int) srcFile.length()]; 
-            byte[] eachContent = new byte[(int) eachFile.length()];
-			try {
-				FileInputStream fis = new FileInputStream(srcFile);
-				fis.read(eachContent);
+		
+		murgeFile("d:/","eclipse.exe");
+	}		
+	private static void murgeFile(String folder,String fileName) {
+		try {
+			File destFile = new File(folder,fileName);
+			int index=0;
+			FileOutputStream fos = new FileOutputStream(destFile);
+			while(true) {
+	            File eachFile = new File(folder,fileName + "-" + index++);
+	            if(!eachFile.exists()) {
+	            	break;
+	            }
+	            byte[] eachContent = new byte[(int) eachFile.length()];
+				FileInputStream fis = new FileInputStream(eachFile);
+				fis.read(eachContent);				
+				fos.write(eachContent);
 				fis.close();
-				FileOutputStream fos = new FileOutputStream(eachFile);
-				fos.write(fileContent);
-	            fos.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}      
-        }
-	}
-
+				fos.flush();
+			}
+            fos.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}      
+    }
 }
