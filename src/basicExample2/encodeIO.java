ef0f1f434107ae0c08703bd2b132f7d28f1d7490
ZhaiDongboBug <18747867831@163.com>
2020-03-29 16:44:40 +0800

    找出已知十六进制对应UTF-8编码的汉字-1
    1.如何创建字符串数组
    String[] str = {, , , ,};
    String   str[] ={, , , ,};//这个还不是很理解
    2.

---
+++ src/basicExample2/encodeIO.java
@@ -0,0 +1,18 @@
+package basicExample2;
+
+import java.util.ArrayList;
+
+public class encodeIO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] str = {"E5", "B1", "8C"};
+		ArrayList<Byte> bytes = new ArrayList<Byte>();
+		for(String s:str) {
+			bytes.
+			s.getBytes();
+		}
+		
+	}
+
+}
