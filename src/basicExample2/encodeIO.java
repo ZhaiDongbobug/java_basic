9b4dd391070a93a51ffa27cc481bf0428e968cdf
ZhaiDongboBug <18747867831@163.com>
2020-03-29 17:15:50 +0800

    找出已知十六进制对应UTF-8编码的汉字-2
    1.java中 byte数组里储存的是byte类型数据吗?
    答案：基本数据类型之间可以自动转换和强制转换

--- src/basicExample2/encodeIO.java
+++ src/basicExample2/encodeIO.java
@@ -1,18 +1,17 @@
 package basicExample2;
 
-import java.util.ArrayList;
+import java.io.UnsupportedEncodingException;
 
 public class encodeIO {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedEncodingException {
 		// TODO Auto-generated method stub
-		String[] str = {"E5", "B1", "8C"};
-		ArrayList<Byte> bytes = new ArrayList<Byte>();
-		for(String s:str) {
-			bytes.
-			s.getBytes();
-		}
-		
+		byte[] bs = new byte[3];
+		bs[0] = (byte) 0xE5;
+		bs[1] = (byte) 0xB1;
+		bs[2] = (byte) 0x8C;
+		String str = new String(bs,"UTF-8");
+		System.out.println("E5 B1 8C 对应的字符是："+str);
 	}
 
 }
