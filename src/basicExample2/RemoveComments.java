74a7ab91fae4b4baa0471de5d5b5abf17c017f85
ZhaiDongboBug <18747867831@163.com>
2020-04-05 20:47:31 +0800

    缓存流-删除注释-2
    1.BufferedReader br = new BufferedReader(fr);
    //这是建立一个读取通道的意思
    那么FileReader 不也是建立一个通道的意思吗？为什么多此一举？
    //FileReader把数据从硬盘读取到内存是以字符为单位的I/O操作较多，读取数据
    //要在通过BuffedReader 读取完数据后，才能建立Printwriter，因为创建输出流的时候，会把目标文件内容清空
    //line.trim()是返回无前导和尾随空白的字符串
    //sb.append()是追加字符串的意思
    //   \r表示回车\n表示换行
    //缓存流必须建立在一个存在的流的基础上

--- src/basicExample2/RemoveComments.java
+++ src/basicExample2/RemoveComments.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class RemoveComments {
 
@@ -16,6 +18,7 @@ public class RemoveComments {
 
 	public static void removeComments(File javaFile) {
 		// TODO Auto-generated method stub
+		StringBuffer sb = new StringBuffer();
 		try(
 				FileReader fr = new FileReader(javaFile);
 				BufferedReader br = new BufferedReader(fr);
@@ -25,12 +28,9 @@ public class RemoveComments {
 				String line = br.readLine();
 				if (null == line)
 					break;
-				int index = line.indexOf("//");
-				if(index > 0) {
-					String newStr = line.substring(0, index);
-				}
-				String[] strA = new 
-				
+				if(!line.trim().startsWith("//")) {
+					sb.append(line).append("\r\n");
+				} 
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -39,6 +39,13 @@ public class RemoveComments {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		try(FileWriter fw = new FileWriter(javaFile);
+			PrintWriter pw = new PrintWriter(fw);
+				){
+			pw.write(sb.toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
