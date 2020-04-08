c5bef19486944986970ce14c8a962adebf7f53f8
ZhaiDongboBug <18747867831@163.com>
2020-04-08 22:07:22 +0800

    序列化一个对象-2
    1.数组创建的几种方式：
    （1）声明数组
    //例子： 声明整型数组
    //int[] intArray0 ;
    //int intArray1 []；
    2.关于类的声明
    //public可以跨类跨包访问
    //protected 可以被本包类访问，其中该类的子类访问可以跨包
    //default 只能在本包中访问
    //private 只能被自身类的对象访问
    3.关于this
    //this指的是当前对象

--- src/basicExample2/hero.java
+++ src/basicExample2/hero.java
@@ -1,8 +1,10 @@
 package basicExample2;
 
-public class hero {
-	public int value;
-	public hero(int value) {
-		this.value = value;
+import java.io.Serializable;
+
+public class hero implements Serializable{
+	String name;
+	public hero(String name) {
+		this.name = name;
 	}
 }
--- src/basicExample2/objectStream.java
+++ src/basicExample2/objectStream.java
@@ -12,9 +12,8 @@ public class objectStream {
 
 	public static void main(String[] args) {
 		hero[] ho = new hero[10];
-		for(int i=0;i<10;i++) {
-			hero h = new hero(i);
-			ho[i] = h;
+		for(int i=0;i<ho.length;i++) {
+			ho[i] = new hero("hero:"+i);
 		}
 		File f = new File("heros.lol");
 		try(FileInputStream fis = new FileInputStream(f);
@@ -23,12 +22,19 @@ public class objectStream {
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
 			){
 			oos.writeObject(ho);
-			hero[] Ho = new hero[10];
-			ois.readObject(Ho);
+			hero[] hs = (hero[]) ois.readObject();
+			System.out.println("查看中文件中反序列化出来的数组中的每一个元素：");
+			for(hero hr:hs) {
+				System.out.println(hr.name);
+			}
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
