e8ee713e2b36f4731e138f8bb28a1f2f79402146
ZhaiDongboBug <18747867831@163.com>
2020-04-12 21:55:31 +0800

    查找文件-2
    1.关于File:
    2.关于system.out.priintf():

--- src/basicExample2/searchString.java
+++ src/basicExample2/searchString.java
@@ -9,41 +9,41 @@ import java.io.IOException;
 public class searchString {
 
 	public static void main(String[] args) {
-		search("e://project","Magic");
+		File Folder = new File("e://project");
+		search(Folder,"Magic");
 	}
 
-	private static void search(String folder, String search) {
-		File Folder = new File(folder);
-		if(!Folder.exists())
-			return;
-		File[] files = Folder.listFiles();
-		for(File file:files) {
-			if(file.isDirectory()) {
-				File newFolder = new File(Folder.getAbsolutePath(),file.getName());
-				search(newFolder.getAbsolutePath(),search);
-			}
-			if(file.length() == 0)
-				continue;
-			if(file.isFile()) {
-				File newFile = new File(Folder.getAbsolutePath(),file.getName());
-				try(FileReader fr = new FileReader(newFile);
-					BufferedReader br = new BufferedReader(fr);	
-						){
-					String s = null;
-					while((s = br.readLine()) != null) {
-						if(s.contains(search))
-							break;
-					}
-					br.close();
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
+	private static void search(File folder, String string) {
+		if(folder.isFile()) {
+			if(folder.getName().toLowerCase().endsWith(".java")) {
+				String fileContent = readFileContent(folder);
+				if(fileContent.contains(string)) {
+					System.out.printf("找到子目标字符串%s,在文件:%s%n",string,folder);
 				}
-				
 			}
 		}
+		if (folder.isDirectory()) {
+            File[] fs = folder.listFiles();
+            for (File f : fs) {
+                search(f, string);
+            }
+        }
 		
+	}
+
+	private static String readFileContent(File folder) {
+		try(FileReader fr = new FileReader(folder);){
+			char[] all = new char[(int) folder.length()];
+			fr.read(all);
+			return new String(all);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
