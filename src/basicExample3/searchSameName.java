8445e37e0d5c35857b00f470d1a8a7179120a354
ZhaiDongboBug <18747867831@163.com>
2020-04-14 21:44:11 +0800

    集合ArrayList之判断是否相同-2
    1.关于equals:
    // ==判断的是值相等 equals判断的是引用指向存储对象的内存地址相等

--- src/basicExample3/searchSameName.java
+++ src/basicExample3/searchSameName.java
@@ -6,12 +6,13 @@ public class searchSameName {
 
 	public static void main(String[] args) {
 		ArrayList<hero> heros = new ArrayList<hero>();
-		heros.add(new hero("hero 1"));
-		heros.add(new hero("hero 2"));
-		heros.add(new hero("hero 3"));
-		heros.add(new hero("hero 4"));
-		for(hero h:heros) {
-			if(h.toString() == "hero 1") {
+		String name = "hero 1";
+		for(int i=0; i<5; i++) {
+			heros.add(new hero("hero "+i));
+		}
+		for(int i=0; i<heros.size(); i++) {
+			hero h = heros.get(i);
+			if(name.equals(h.toString())) {
 				System.out.println("找到了name是\"hero 1\"的对象 ");
 				break;
 			}
