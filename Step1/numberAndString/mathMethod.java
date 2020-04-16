cc1a6e0d94ebe3614ae2cee2e97e21a591ceb9d2
ZhaiDongboBug <18747867831@163.com>
2020-04-16 20:55:14 +0800

    数学方法-自然数
    1.关于System.out.println()
    //System是java.lang里面的一个类
    //out是System里面的一个静态数据成员
    //println()是java.io.PrintStream类里的一个方法
    //System.out.println()参数可以是Object类

---
+++ Step1/numberAndString/mathMethod.java
@@ -0,0 +1,12 @@
+package numberAndString;
+
+public class mathMethod {
+
+	public static void main(String[] args) {
+		System.out.println(Math.E);
+		int n = Integer.MAX_VALUE;
+		System.out.println(Math.pow((1+1d/n), n));
+		
+	}
+
+}
