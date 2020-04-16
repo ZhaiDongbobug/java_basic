c34326584b2eb7ab37670ff943e6cfae8540825c
ZhaiDongboBug <18747867831@163.com>
2020-04-16 15:07:07 +0800

    装箱和拆箱-2
    1.Integer类 不用声明就可以直接使用吗？
    //Integer是隐式声明

--- Step1/numberAndString/upOrDownBox.java
+++ Step1/numberAndString/upOrDownBox.java
@@ -3,27 +3,28 @@ package numberAndString;
 public class upOrDownBox {
 
 	public static void main(String[] args) {
-		byte b = 12;
-		Byte B = b;
-		b = B;
+		byte b = 1;
+		short s = 2;
+		float f = 3.14f;
+		double d = 6.18;
 		
-		short s = 63;
-		Short S = s;
-		s = S;
+		//自动装箱
+		Byte b1 = b;
+		Short s1 = s;
+		Float f1 = f;
+		Double d1 = d;
+		//自动拆箱
+		b=b1;
+		s=s1;
+		f=f1;
+		d=d1;
 		
-		float f = 2^16-1;
-		Float F = f;
-		f = F;
+		//2.byte和Integer之间能否进行自动拆箱和自动装箱
+		Integer i1 = b;//不能把byte自动装箱成Integer
+		b = new Integer(1);//不能把Integer自动拆箱成byte
 		
-		double d = 2^32-1;
-		Double D = d;
-		d = D;
-		
-		Integer I = (int) b;
-		int i = 16;
-		B = (byte) i;
-		
-		b = B.MAX_VALUE;
+		//3. 通过Byte获取byte的最大值
+		System.out.println(Byte.MAX_VALUE);
 		
 	}
 
