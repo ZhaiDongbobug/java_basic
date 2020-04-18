1916e0d6d85acd264865582f842fe4829e8c1899
ZhaiDongboBug <18747867831@163.com>
2020-04-18 08:53:06 +0800

    字符练习-2
    1.关于System.out.print()
    
    //System.out.print();与System.out.println();
    前者输出不换行，比如：
    System.out.print("a");
    System.out.print("b");
    结果：
    ab
    
    后者输出后换行，比如：
    System.out.println("a");
    System.out.println("b");
    结果：
    a
    b
    2.今天get到的新技能
    //无论你的代码有多乱，Ctrl+Shift+F,立刻规范代码

--- Step1/numberAndString/characterExample.java
+++ Step1/numberAndString/characterExample.java
@@ -1,24 +1,17 @@
 package numberAndString;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class characterExample {
 
 	public static void main(String[] args) {
-		ArrayList all = new ArrayList();
 		Scanner s = new Scanner(System.in);
-		String str = s.next();
+		String str = s.nextLine();
 		char[] cs = str.toCharArray();
-		for(char ch:cs) {
-			Character ca = ch;
-			if(ca.isDigit(ch)||ca.isUpperCase(ch)) {
-				all.add(ch);
+		for (char ch : cs) {
+			if (Character.isDigit(ch) || Character.isUpperCase(ch)) {
+				System.out.print(ch);
 			}
 		}
-		String str2 = all.toString();
-		System.out.println(str2);
-
 	}
-
 }
