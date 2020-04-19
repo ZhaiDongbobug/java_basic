464d0ea2942c9cb572ddc6c9de4ac778d6785740
ZhaiDongboBug <18747867831@163.com>
2020-04-19 09:28:12 +0800

    随机字符-2
    1.一个double数强制转换为int数会发生什么？
    //如果浮点数比整数类型能表示的最大值还大，那么结果就是整数类型的最大值
    //如果flaot比int最大值小，那么去掉小数部分就是所转换的结果

--- Step1/numberAndString/createRandomString.java
+++ Step1/numberAndString/createRandomString.java
@@ -5,27 +5,40 @@ import java.util.Random;
 public class createRandomString {
 
 	public static void main(String[] args) {
-		char[] ch = new char[5];
-		for (int i = 0; i < 5; i++) {
-			Random random = new Random();
-			String numOrChar = random.nextInt(2) == 0 ? "number" : "char";
-			if ("char".equals(numOrChar)) {
-				String upOrLowCase = random.nextInt(2) == 0 ? "upperCase" : "lowerCase";
-				if ("lowerCase".equals(upOrLowCase)) {
-					char ranv = (char) (random.nextInt(26) + 97);
-					ch[i] = ranv;
-				} else {
-					char ranc = (char) (random.nextInt(26) + 65);
-					ch[i] = ranc;
+		// 方法一
+		char[] cs = new char[5];
+		short start = '0';
+		short end = 'z' + 1;
+		for (int i = 0; i < cs.length; i++) {
+			while (true) {
+				char c = (char) (Math.random() * (end - start) + start);
+				if (Character.isDigit(c) || Character.isLetter(c)) {
+					cs[i] = c;
+					break;
 				}
-			} else {
-				int ran = random.nextInt(10) + 48;
-				ch[i] = (char) ran;
 			}
-
 		}
-		String str = new String(ch);
-		System.out.println(str);
+		String result = new String(cs);
+		System.out.println(result);
+		// 方法二
+		String pool = "";
+		for (int i = '0'; i < '9' + 1; i++) {
+			pool += (char) i;
+		}
+		for (int i = 'A'; i < 'Z' + 1; i++) {
+			pool += (char) i;
+		}
+		for (int i = 'a'; i < 'z' + 1; i++) {
+			pool += (char) i;
+		}
+		char[] cs2 = new char[5];
+
+		for (int i = 0; i < cs2.length; i++) {
+			int index = (int) (Math.random() * pool.length());
+			cs2[i] = pool.charAt(index);
+		}
+		String result2 = new String(cs2);
+		System.out.println(result2);
 	}
 
 }
