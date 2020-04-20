bfd80c4382da0f88391a4036f3435bd201e8889b
ZhaiDongboBug <18747867831@163.com>
2020-04-20 17:27:44 +0800

    字符串数组排序-2
    1.关于冒泡排序
    //使顺序从小到大排列，相当于气泡往上升

--- Step1/numberAndString/StrArrSort.java
+++ Step1/numberAndString/StrArrSort.java
@@ -1,40 +1,49 @@
 package numberAndString;
 
+import java.util.Arrays;
+
 public class StrArrSort {
 
 	public static void main(String[] args) {
 		String[] strArr = new String[8];
-		for(int i=0;i<strArr.length;i++) {
-			strArr[i] = getString();
+		for (int i = 0; i < strArr.length; i++) {
+			strArr[i] = getString(5);
 		}
-		short cr;
-		for(int i=1;i<strArr.length;i++) {
-			char chr = strArr[i+1].charAt(0);
-			Character.isUpperCase(chr)
-			if(strArr[i+1].charAt(0)<strArr[i].charAt(0)) {
-				cr = (short) strArr[i+1].charAt(0);				
-				String med = strArr[i];
-				strArr[i] = strArr[i+1];
-				strArr[i+1] = med;
-			}
-			
-		}
-		
-
-	}
-
-	private static String getString() {
-		char[] cs = new char[5];
-		short start = 'A';
-		short end = 'z' + 1;
-		for (int i = 0; i < cs.length; i++) {
-			while (true) {
-				char c = (char) (Math.random() * (end - start) + start);
-				if (Character.isLetter(c)) {
-					cs[i] = c;
-					break;
+		System.out.println("未排序前的字符串数组：");
+		System.out.println(Arrays.toString(strArr));
+		for (int j = 0; j < strArr.length; j++) {
+			for (int i = 1; i < strArr.length - j - 1; i++) {
+				char firstChar1 = strArr[i].charAt(0);
+				char firstChar2 = strArr[i + 1].charAt(0);
+				firstChar1 = Character.toLowerCase(firstChar1);
+				firstChar2 = Character.toLowerCase(firstChar2);
+				if (firstChar1 > firstChar2) {
+					String med = strArr[i];
+					strArr[i] = strArr[i + 1];
+					strArr[i + 1] = med;
 				}
 			}
+		}
+		System.out.println("排序后的字符串数组：");
+		System.out.println(Arrays.toString(strArr));
+	}
+
+	private static String getString(int length) {
+		String pool = "";
+		for (int i = '0'; i < '9' + 1; i++) {
+			pool += (char) i;
+		}
+		for (int i = 'A'; i < 'Z' + 1; i++) {
+			pool += (char) i;
+		}
+		for (int i = 'a'; i < 'z' + 1; i++) {
+			pool += (char) i;
+		}
+		char[] cs = new char[length];
+
+		for (int i = 0; i < cs.length; i++) {
+			int index = (int) (Math.random() * pool.length());
+			cs[i] = pool.charAt(index);
 		}
 		String result = new String(cs);
 		return result;
