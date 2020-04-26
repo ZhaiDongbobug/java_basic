f61b306d3b86c65c84eeae47a04f14c090256c46
ZhaiDongboBug <18747867831@163.com>
2020-04-26 16:42:01 +0800

    穷举法破解随机字符串密码-3
    1.注意：类方法不能调用main()函数里定义的变量，除非作为方法的参数
    2.注意：类静态方法只能调用【静态】成员变量
    3.StackOverflowError爆出异常，在下才疏学浅，搞不清该如何解决了。

--- Step1/numberAndString/bruForceCode.java
+++ Step1/numberAndString/bruForceCode.java
@@ -1,40 +1,52 @@
 package numberAndString;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class bruForceCode {
 
+	static String code;
+	static int index = 0;
 	public static void main(String[] args) {
-		Boolean flag = false;
-		String code = getString(3);
-		System.out.println("随机取得的密码串：" + code);
-		for (int i = '0'; i < 'z' + 1; i++) {
-			if (flag) {
-				break;
-			}				
+		code = getString(3);
+		String recode = "";
+		ArrayList<String> strArry = strpool();
+		searchRecode(recode,strArry);
+	}
+	
+	private static ArrayList<String> strpool() {
+		ArrayList<String> str = new ArrayList<String>();
+		for (int i = '0'; i < 'z' + 1; i++) {				
 			for (int j = '0'; j < 'z' + 1; j++) {
-				if (flag) {
-					break;
-				}
 				for (int k = '0'; k < 'z' + 1; k++) {	
-					
 					char[] recode = new char[3];
 					recode[0] = (char)i;
 					recode[1] = (char)j;
 					recode[2] = (char)k;
-					StringBuffer str = new StringBuffer();
-					str.append(recode);
-					String strRecode = str.toString();
-					//String strRecode = Arrays.toString(recode);
-					//System.out.println(strRecode);
-					if (code.equals(strRecode)) {
-						System.out.println("经多层for循环得到的解码：" + strRecode);
-						flag = true;
-						break;
-					}
+					StringBuffer sbf = new StringBuffer();
+					sbf.append(recode);
+					String s = sbf.toString();
+					str.add(s);
 				}
 			}
 		}
+		return str;
+	}
+	private static String getStr
+	(int index,ArrayList<String> strArry) {	
+		return strArry.get(index);
+	}
+	private static void searchRecode
+	(String recode,ArrayList<String> strArry) {
+		if(recode.equals(code)) {
+			System.out.println("解码成功，密码：" + recode);
+		}else {
+			recode = getStr(index,strArry);
+			index++;
+			System.out.println(recode);
+			searchRecode(recode,strArry);
+		}
+			
+		
 	}
 
 	private static String getString(int length) {
