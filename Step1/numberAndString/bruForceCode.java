889e96f8fce7bf954d56169e16de1646eadd773d
ZhaiDongboBug <18747867831@163.com>
2020-04-27 16:06:59 +0800

    穷举法破解随机字符串密码-5
    1.short和char虽然都有两个字节的长度，但short表示的是16位有符号整数，char表示的是16位无符号整数。char用来保存一个Unicode编码的字符。char和short之间类型转换需要强转。
    
    2.short类型的变量，i++和i+=1操作是正确的，因为并没有改变i的数据类型。但i=i+1会编译错误，表达式右边要求强制类型转换。前面两个操作之所以通过，是因为复合赋值自带了隐式的强制类型转换。
    总结：
    （1）使用for循环遍历三个字符组成字符串的所有可能性
    （2）定义char数组，将char数组用new String()转换为字符串，再用equals判断

--- Step1/numberAndString/bruForceCode.java
+++ Step1/numberAndString/bruForceCode.java
@@ -3,55 +3,36 @@ package numberAndString;
 import java.util.ArrayList;
 
 public class bruForceCode {
-
-	static String code;
-	static int index = 0;
 	public static void main(String[] args) {
-		code = getString(3);
-		System.out.println("随机得到的密码为：" + code);
-		String recode = "";
-		ArrayList<String> strArry = strpool();
-		searchRecode(recode,strArry);
-	}
-	//创建一个String的池子，里面包含了密码的所有可能
-	private static ArrayList<String> strpool() {
-		ArrayList<String> str = new ArrayList<String>();
-		for (int i = '0'; i < 'z' + 1; i++) {				
-			for (int j = '0'; j < 'z' + 1; j++) {
-				for (int k = '0'; k < 'z' + 1; k++) {	
-					char[] recode = new char[3];
-					recode[0] = (char)i;
-					recode[1] = (char)j;
-					recode[2] = (char)k;
-					StringBuffer sbf = new StringBuffer();
-					//这里频繁创建StringBuffer对象
-					sbf.append(recode);
-					String s = sbf.toString();
-					str.add(s);
+		String password = randomString(3);
+		System.out.println("密码是：" + password);
+		char[] guessPassword = new char[3];
+		outloop:
+			for(short i='0';i<='z';i++) {
+				for(short j='0';j<='z';j++) {
+					for(short k='0';k<='z';k++) {
+						if(!isLetterOrDigital(i,j,k))
+							continue;
+						guessPassword[0] = (char)i;
+						guessPassword[1] = (char)j;
+						guessPassword[2] = (char)k;
+						String guess = new String(guessPassword);
+						if(guess.equals(password)) {
+							System.out.println("找到了，密码是：" + guess);
+							break outloop;
+					}
 				}
 			}
 		}
-		return str;
 	}
-	private static String getStr
-	(int index,ArrayList<String> strArry) {	
-		return strArry.get(index);
+	private static boolean isLetterOrDigital(short i, short j, short k) {
+		if(Character.isLetterOrDigit(i)
+				&&Character.isLetterOrDigit(j)
+				&&Character.isLetterOrDigit(k))
+		return true;
+		return false;
 	}
-	private static void searchRecode
-	(String recode,ArrayList<String> strArry) {
-		if(recode.equals(code)) {
-			System.out.println("解码成功，密码：" + recode);
-		}else {
-			recode = getStr(index,strArry);
-			index++;
-			System.out.println(recode);
-			searchRecode(recode,strArry);
-		}
-			
-		
-	}
-
-	private static String getString(int length) {
+	private static String randomString(int length) {
 		String pool = "";
 		for (int i = '0'; i < '9' + 1; i++) {
 			pool += (char) i;
