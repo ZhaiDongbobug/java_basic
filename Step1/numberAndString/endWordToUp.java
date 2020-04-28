37654b75b2f14fca01baa8bda706db394d9e7a42
ZhaiDongboBug <18747867831@163.com>
2020-04-28 22:22:16 +0800

    1.最后一个字母变大写-1
    总结：
    找出最后一个字符将其大写，另外将字符串截取一个去掉最后字符的子字符串，两者相加，输出，

---
+++ Step1/numberAndString/endWordToUp.java
@@ -0,0 +1,16 @@
+package numberAndString;
+
+public class endWordToUp {
+
+	public static void main(String[] args) {
+		String sentence = "lengendary";
+		int endIndex = sentence.length();
+		char c = Character.
+				toUpperCase(sentence.charAt
+						(endIndex-1));
+		String rest = sentence.substring(0, endIndex-2);
+		String result = rest + c;
+		System.out.println(result);
+	}
+
+}
