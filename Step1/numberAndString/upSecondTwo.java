62eadcd9b54a03fbbb8d769193683e96219a3f41
ZhaiDongboBug <18747867831@163.com>
2020-05-02 10:34:51 +0800

    第二个two首字母大写-2

--- Step1/numberAndString/upSecondTwo.java
+++ Step1/numberAndString/upSecondTwo.java
@@ -7,7 +7,10 @@ public class upSecondTwo {
 				+ "two eyes, and but one tongue, to the "
 				+ "end that we should hear and see more "
 				+ "than we speak";
-		
+		int index = sentence.lastIndexOf(" two ");
+		char[] senArry = sentence.toCharArray();
+		senArry[index + 1] = Character.toUpperCase(senArry[index+1]);
+		String result = new String(senArry);
+		System.out.println(result);
 	}
-
 }
