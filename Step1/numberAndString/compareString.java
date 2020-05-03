6d56aafde415aa9eb62dbe42d4574fb40b2e3060
ZhaiDongboBug <18747867831@163.com>
2020-05-03 10:48:30 +0800

    比较字符串-2
    1.控制台输出[Ljava.lang.String;@106d69c
    原因：System.out.println();默认输出对象的toString(); 除了char[] 其他类型数组直接输出 不行
    [ 是一维数组 L 是对象类型 java.lang.String;对象的标准类名 @106d69c 内存地址
    2.java.lang.ArrayIndexOutOfBoundsException搞不明白为啥第23行会爆出这个错误，哪里越界了，不明白。
    
    总结：我的思路是用printf来输出随机得到的字符串数组，String.equals()方法比较判断出重复的字符串，用ArrayList找出重复3次以上的字符串，这是for循环找不出来的。

--- Step1/numberAndString/compareString.java
+++ Step1/numberAndString/compareString.java
@@ -1,18 +1,40 @@
 package numberAndString;
 
+import java.util.ArrayList;
+
 public class compareString {
 
 	public static void main(String[] args) {
 		String[] strArry = new String[100];
 		for(int i=0;i<strArry.length;i++) {
 			strArry[i] = randomString(2);
+			
 		}
-		String str = strArry.toString();
-		System.out.println(str);
-		int count = 0;
-
+		//System.out.println(strArry.length);
+		for(int i=0;i<strArry.length;i++) {
+			System.out.printf("%-3s",strArry[i]);
+			if((i+1)%20 == 0) {
+				System.out.printf("\n");
+			}
+		}
+		ArrayList<String> alStr = new ArrayList<String>(); 
+		for(int i=0;i<strArry.length;i++) {
+			for(int j=0;i<strArry.length;j++) {
+				if(strArry[i].equals(strArry[j])) {
+					alStr.add(strArry[i]);
+				}
+			}
+		}
+		ArrayList<String> tempList = new ArrayList<String>(alStr.size());
+		for(int i=0;i<alStr.size();i++) {
+			if(!tempList.contains(alStr.get(i))) {
+				tempList.add(alStr.get(i));
+			}
+		}
+		System.out.printf("总共有%d种重复的字符串\n",tempList.size());
+		System.out.printf("分别是：\n");
+		System.out.printf("%-3s",tempList.listIterator());
 	}
-
 	private static String randomString(int length) {
 		String pool = "";
 		for (int i = '0'; i < '9' + 1; i++) {
