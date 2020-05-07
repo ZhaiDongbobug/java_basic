99ab0a12602974785c1a2a4a06f1dac1e0d0c901
ZhaiDongboBug <18747867831@163.com>
2020-05-07 21:44:35 +0800

    自建StringBuffer类-2
    对插入和构造方法的一些改动
    使用for循环将要插入的位置后半部向后移动

--- Step1/numberAndString/MyStringBuffer.java
+++ Step1/numberAndString/MyStringBuffer.java
@@ -5,26 +5,31 @@ public class MyStringBuffer implements IStringBuffer{
 	int capacity = 16;
 	int length = 0;
 	char[] value;
+	
 	public MyStringBuffer() {
 		value = new char[capacity];
 	}
 	public MyStringBuffer(String str) {
-		if(null != str) {
-			value = str.toCharArray();
-		}
-		length = value.length;
-		if(capacity<value.length) {
+		this();
+		if(null == str) {
+			return;
+		}	
+		if(capacity<str.length()) {
 			capacity = value.length*2;
+			value = new char[capacity];
 		}
+		if(capacity>=str.length())
+			System.arraycopy(str.toCharArray(), 0, value, 0, str.length());
+		length = str.length();
 	}
 	public void append(String str) {
 		if(null != str) {
 			char[] newValue = str.toCharArray();
-			if(capacity<newValue.length) {
-				capacity = newValue.length*2;
+			if(capacity-length < newValue.length) {
+				capacity += newValue.length*2;
 			}
-			length = newValue.length;
-			System.arraycopy(newValue, 0, value, 0, length);
+			System.arraycopy(newValue, 0, value, 0, newValue.length);
+			length = value.length;
 		}	
 	}
 	@Override
@@ -39,8 +44,23 @@ public class MyStringBuffer implements IStringBuffer{
 	}
 	@Override
 	public void insert(int pos, String b) {
-		// TODO Auto-generated method stub
-		
+		if(pos<0)
+			return;
+		if(b == null)
+			return;
+		if(b.length()>(capacity-length)) {
+			capacity += b.length()*2;
+			char[] value = new char[capacity];
+		}
+		if(pos<length) {
+			for(int i=0;i<length-pos;i++) {
+				char temp = value[length-1-i];
+				value[length-1-i] = value[b.length()+length-1-i];
+				value[b.length()+length-1-i] = temp;
+			}
+		}
+		System.arraycopy(b.toCharArray(),0, value, pos, b.toCharArray().length);
+		length += b.length(); 
 	}
 	@Override
 	public void delete(int start) {
@@ -54,9 +74,8 @@ public class MyStringBuffer implements IStringBuffer{
 	}
 	@Override
 	public void reverse() {
-		char temp;
-		for(int i=0;i<length;i++) {
-			temp = value[i];
+		for(int i=0;i<length/2;i++) {
+			char temp = value[i];
 			value[i] = value[length-i-1];
 			value[length-i-1] = temp;
 		}
@@ -65,5 +84,17 @@ public class MyStringBuffer implements IStringBuffer{
 	public int length() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	public String toString() {
+		char[] realValue = new char[length];
+		System.arraycopy(value, 0, realValue, 0, length);
+		return new String(realValue);
+	}
+	
+	public static void main(String[] args) {
+		MyStringBuffer sb = new MyStringBuffer("there light");
+		sb.insert(12, "000");
+		System.out.println(sb);
 	}
 }
