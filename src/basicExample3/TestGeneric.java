f894c4baa3e2cb5ba1e4307b68a63bd3ca4d1c8f
ZhaiDongboBug <18747867831@163.com>
2020-05-10 22:38:44 +0800

    泛型
    总结：
    泛型的用法是在容器后面添加<Type>，Type可以是类，抽象类，接口。
    它们的子类对象也可以放进容器。

---
+++ src/basicExample3/TestGeneric.java
@@ -0,0 +1,18 @@
+package basicExample3;
+
+import java.util.ArrayList;
+
+public class TestGeneric {
+
+	public static void main(String[] args) {
+		ArrayList<Number> numbers = new ArrayList<>();
+		numbers.add(123);//int
+        numbers.add(123f);//float
+        numbers.add(123d);//double
+         
+        //numbers.add("String");
+	}
+	
+	
+	
+}
