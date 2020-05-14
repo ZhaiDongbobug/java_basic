e549ff3c5d41e3ea60230523469f587b33b68e94
ZhaiDongboBug <18747867831@163.com>
2020-05-14 22:51:38 +0800

    比较冒泡法、选择法以及二叉树排序法的性能区别-2
    总结：
    1.关于向上转型和向下转型
    向上转型：父类声明的子类对象（Father f = new
    Son()）;作用：一个方法的参数是父类，调用时实际是子类，避免了因参数问题而写大量的方法；
    向下转型：子类声明的父类对象
    （Son s = (Son) f）;作用：找回因向上转型丢失的子类特有方法。
    2.关于Node类中values()方法中的递归理解：
    return的values作为参数又放到addAll方法中，类似重复value += value;
    3.关于排序二叉树静态类中values()方法：
    首先我们要知道，Node类中存放的数据是Object类型的，我们要将它转换为int类型，本来强转是不可以的，但因为属于向下转型就被允许了。

--- src/basicExample3/CompareArithmetic.java
+++ src/basicExample3/CompareArithmetic.java
@@ -1,64 +1,140 @@
 package basicExample3;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CompareArithmetic {
 
 	public static void main(String[] args) {
-		System.out.println("初始化一个长度是40000的随机数字的数组");
+		int total = 40000;
+		System.out.println("初始化一个长度是" + total + "的随机数字的数组");
+		int[] originalNumbers = new int[total];
+		for (int i = 0; i < total; i++) {
+			originalNumbers[i] = (int) (Math.random() * total);
+		}
+		System.out.println("初始化完毕");
+		System.out.println("接下来分别用3种算法");
+		int[] use4sort;
+		use4sort = Arrays.copyOf(originalNumbers, originalNumbers.length);
+		int[] sortedNumbersBySelection = performance(new SelectionSort(use4sort), "选择法");
+
+		use4sort = Arrays.copyOf(originalNumbers, originalNumbers.length);
+		int[] sortedNumbersByBubbling = performance(new BubblingSort(use4sort), "冒泡法");
+
+		use4sort = Arrays.copyOf(originalNumbers, originalNumbers.length);
+		int[] sortedNumbersByTree = performance(new TreeSort(use4sort), "二叉树");
+
+		System.out.println("查看排序结果，是否是不同的数组对象");
+		System.out.println(sortedNumbersBySelection);
+		System.out.println(sortedNumbersByBubbling);
+		System.out.println(sortedNumbersByTree);
+
+		System.out.println("查看排序结果，内容是否相同");
+		System.out.println("比较 选择法 和 冒泡法  排序结果：");
+		System.out.println(Arrays.equals(sortedNumbersBySelection, sortedNumbersByBubbling));
+		System.out.println("比较 选择法 和 二叉树  排序结果：");
+		System.out.println(Arrays.equals(sortedNumbersBySelection, sortedNumbersByTree));
+
+	}
+
+	interface Sort {
+		void sort();
+
+		int[] values();
+	}
+
+	static class SelectionSort implements Sort {
+		int[] numbers;
+
+		SelectionSort(int[] numbers) {
+			this.numbers = numbers;
+		}
+
+		@Override
+		public void sort() {
+			for (int i = 0; i < numbers.length - 1; i++) {
+				for (int j = i + 1; j < numbers.length; j++) {
+					if (numbers[i] > numbers[j]) {
+						int temp = numbers[i];
+						numbers[i] = numbers[j];
+						numbers[j] = temp;
+					}
+				}
+			}
+
+		}
+
+		@Override
+		public int[] values() {
+			// TODO Auto-generated method stub
+			return numbers;
+		}
+
+	}
+
+	static class BubblingSort implements Sort {
+		int[] numbers;
+
+		BubblingSort(int[] numbers) {
+			this.numbers = numbers;
+		}
+
+		@Override
+		public void sort() {
+			for (int i = 0; i < numbers.length; i++) {
+				for (int j = 0; j < numbers.length - i - 1; j++) {
+					if (numbers[j] > numbers[j + 1]) {
+						int temp = numbers[j];
+						numbers[j] = numbers[j + 1];
+						numbers[j + 1] = temp;
+					}
+				}
+			}
+
+		}
+
+		@Override
+		public int[] values() {
+			// TODO Auto-generated method stub
+			return numbers;
+		}
+
+	}
+
+	static class TreeSort implements Sort {
+		int[] numbers;
+		Node n;
+
+		TreeSort(int[] numbers) {
+			n = new Node();
+			this.numbers = numbers;
+		}
+
+		@Override
+		public void sort() {
+			for (int number : numbers) {
+				n.add(number);
+			}
+		}
+
+		@Override
+		public int[] values() {
+			List<Object> list = n.values();
+			int sortedNumbers[] = new int[list.size()];
+			for (int i = 0; i < sortedNumbers.length; i++) {
+				sortedNumbers[i] = (int) list.get(i);
+				// 因为这是向下转型，所以Object可以强转为int类型
+			}
+			return sortedNumbers;
+		}
+	}
+
+	private static int[] performance(Sort algorithm, String type) {
 		long start = System.currentTimeMillis();
-		CompareArithmetic sort = new CompareArithmetic();
-		int[] value1 = sort.initalizeArray(40000);
-		sort.selectSort(value1);
+		algorithm.sort();
+		int sortedNumbers[] = algorithm.values();
 		long end = System.currentTimeMillis();
-		System.out.printf("选择排序，一共耗时%d毫秒\n", end - start);
-		start = System.currentTimeMillis();
-		int[] value2 = sort.initalizeArray(40000);
-		sort.bubbSort(value2);
-		end = System.currentTimeMillis();
-		System.out.printf("冒泡排序，一共耗时%d毫秒\n", end - start);
-		start = System.currentTimeMillis();
-		int[] value3 = sort.initalizeArray(40000);
-		sort.treeSort(value3);
-		end = System.currentTimeMillis();
-		System.out.printf("二叉树排序，一共耗时%d毫秒\n", end - start);
+		System.out.printf("%s排序，一共耗时%d毫秒%n", type, end - start);
+		return sortedNumbers;
 	}
-
-	private int[] initalizeArray(int length) {
-		int[] sct = new int[length];
-		for (int i = 0; i < sct.length; i++) {
-			sct[i] = (int) (Math.random() * length);
-		}
-		return sct;
-	}
-
-	private void treeSort(int[] value3) {
-		TreeNode intTree = new TreeNode();
-		for (int value : value3) {
-			intTree.add(Integer.valueOf(value));
-		}
-	}
-
-	private void bubbSort(int[] sct) {
-		for (int i = 0; i < sct.length; i++) {
-			for (int j = 0; j < sct.length - i - 1; j++) {
-				if (sct[j] > sct[j + 1]) {
-					int temp = sct[j];
-					sct[j] = sct[j + 1];
-					sct[j + 1] = temp;
-				}
-			}
-		}
-	}
-
-	private void selectSort(int[] sct) {
-		for (int i = 0; i < sct.length; i++) {
-			for (int j = i + 1; j < sct.length; j++) {
-				if (sct[i] > sct[j]) {
-					int temp = sct[i];
-					sct[i] = sct[j];
-					sct[j] = temp;
-				}
-			}
-		}
-	}
-
 }
