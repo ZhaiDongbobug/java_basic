efbb6af584aae78decc5ed8686231d72c0aa5a35
ZhaiDongboBug <18747867831@163.com>
2020-05-18 10:29:33 +0800

    HashMap的性能-2

--- src/basicExample3/CompareArrayListAndHashMap.java
+++ src/basicExample3/CompareArrayListAndHashMap.java
@@ -2,6 +2,7 @@ package basicExample3;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class CompareArrayListAndHashMap {
 
@@ -24,16 +25,29 @@ public class CompareArrayListAndHashMap {
 		System.out.println(count);
 		long end = System.currentTimeMillis();
 		System.out.printf("ArrayList耗费的时间为%d毫秒%n",end-start);
-		HashMap<String,Hero> value2 = new HashMap<>();
+		HashMap<String,List<Hero>> value2 = new HashMap<>();
+		//定义字符串键值key
 		String str = null;
+		//定义存储相同Hero对象的value值list
+		List<Hero> list;
 		for(int i=0;i<total;i++) {
 			int number = getNumber();
 			str = "hero-"+number;
-			value2.put(str, new Hero(str));
+			//判断HashMap里是否有这个键值，没有创建新的集合放相同的Hero对象
+			if(value2.get(str) == null) {
+				list = new ArrayList<>();
+				list.add(new Hero(str));
+				value2.put(str, list);
+			}else {
+				//如果有这个键值不为空，那么向list中添加对象
+				list = value2.get(str);
+				list.add(new Hero(str));
+				value2.put(str, list);
+			}		
 		}
 		start = System.currentTimeMillis();
-		System.out.println(value2.get("hero-5555").name);
-		value2.get("hero-5555");
+		List<Hero> heroList = value2.get("hero-5555");
+		System.out.println(heroList);
 		end = System.currentTimeMillis();
 		System.out.printf("HashMap耗费的时间为%d毫秒",end-start);
 	}
