8195f78fdbc40215177e0c23d133274e42dec46a
ZhaiDongboBug <18747867831@163.com>
2020-05-23 16:35:22 +0800

    HashMap之反转key和value-1
    总结：
    
    1.HashMap扩容是新建了一个HashMap的底层数组，然后调用transfer方法，将HashMap的全部元素添加到新的HashMap中，扩容是一个相当耗费时间的操作，最好提前预估下HashMap中元素的个数，这样有利于提高HashMap的性能。
    2.HashTable直接使用对象的hashCode,而HashMap重新计算hash值。
    3.HashMap、 HashSet、 HashTable 等 基于哈希存储方式的集合是无序的。其它的集合都是有序的。
    而TreeMap TreeSet 等集合是排序的
    4.
    介绍两个方法，一个返回Map的所有key,一个返回Map的所有value,但这两个方法返回值都只是映射，不能随便对其操作，会影响Map
    hashMap.keySet()  return Set
    //因为键不能重复，所以返回的Set
    hashMap.values()  return Conllection
    //因为值可以重复，所以返回的是Collection
    4.异常
    java.util.ConcurrentModificationException
    Iterator 是工作在一个独立的线程中，Iterator
    被创建之后会建立一个指向原来对象的单链索引表，当原来的对象数量发生变化时，这个索引表的内容不会同步改变，所以当索引指针往后移动的时候就找不到要迭代的对象，所以按照
    fail-fast 原则 Iterator 会马上抛出 java.util.ConcurrentModificationException 异常

---
+++ src/basicExample3/HashTableTest.java
@@ -0,0 +1,26 @@
+package basicExample3;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class HashTableTest {
+
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<>(4);
+		Map<String, String> newMap = new HashMap<>(4);
+		map.put("adc", "物理英雄");
+		map.put("apc", "魔法英雄");
+		map.put("t", "坦克");
+		System.out.println(map);
+		Set<String> set = map.keySet();
+		for (Iterator<String> iterator = set.iterator(); iterator.hasNext();) {
+			String key = iterator.next();
+			String value = map.get(key);
+			map.put(value, key);
+		}
+		System.out.println(newMap);
+	}
+
+}
