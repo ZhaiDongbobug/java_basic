a71fa81e8c0048f631971c1faf3c29fd2ae78c76
ZhaiDongboBug <18747867831@163.com>
2020-05-23 10:07:37 +0800

    Collecetions练习-1
    总结：
    1.ArrayList的subList方法
    ArrayList 的 subList 结果不可强转成 ArrayList ，否则会抛出 ClassCastException异常： java
    . util . RandomAccessSubList cannot be cast to java . util . ArrayList ;
    说明： subList 返回的是  ArrayList 的内部类  SubList ，并不是  ArrayList ，而是ArrayList
    的一个视图，对于 SubList 子列表的所有操作最终会反映到原列表上
    2.从网上复制了一个计算百分比的方法，低级程序员的复制粘贴的绝活，哭！！！什么时候我能成为大佬呀。

---
+++ src/basicExample3/CollectionsTest.java
@@ -0,0 +1,39 @@
+package basicExample3;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionsTest {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			list.add(i);
+		}
+		double total = 1000 * 1000;
+		double count = 0;
+		for (int i = 0; i < total; i++) {
+			Collections.shuffle(list);
+			if (list.get(0) == 3 && list.get(1) == 1 && list.get(2) == 4) {
+				count++;
+			}
+		}
+		String result = accuracy(count, total, 3);
+		System.out.println(result);
+
+	}
+
+	public static String accuracy(double num, double total, int scale) {
+		DecimalFormat df = (DecimalFormat) NumberFormat.getInstance();
+		// 可以设置精确几位小数
+		df.setMaximumFractionDigits(scale);
+		// 模式 例如四舍五入
+		df.setRoundingMode(RoundingMode.HALF_UP);
+		double accuracy_num = num / total * 100;
+		return df.format(accuracy_num) + "%";
+	}
+}
