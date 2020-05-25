dc456ad787b3309afafc69d277633b2000ca724b
ZhaiDongboBug <18747867831@163.com>
2020-05-25 19:14:14 +0800

    MyHashMap内容查找性能比较-2

--- src/basicExample3/MyHashMapCompare.java
+++ src/basicExample3/MyHashMapCompare.java
@@ -7,12 +7,30 @@ public class MyHashMapCompare {
 
 	public static void main(String[] args) {
 		int total = 100 * 1000;
-		List<Hero> list = new ArrayList<>(total);
+		List<Hero> list = new ArrayList<>(total);//避免多次扩容浪费时间
+		System.out.println("初始化开始");
 		for (int i = 0; i < total; i++) {
 			int number = (int) (Math.random() * 8999 + 1000);
 			list.add(new Hero("hero-" + number));
+		}	
+		//名字作为key
+		//名字相同的hero,放在一个List中,作为value
+		MyHashMap map = new MyHashMap();
+		for (Hero hero : list) {
+			List<Hero> value = (List<Hero>) map.get(hero.name);
+			if (value == null) {
+				value = new ArrayList();
+				map.put(hero.name, value);
+			}
+			value.add(hero);		
 		}
-		// 强for循环遍历
+		System.out.println("初始化结束");
+		System.out.println("开始查找");
+		findByIterator(list);
+		findByMyHashMap(map);
+	}
+	
+	private static List<Hero> findByIterator(List<Hero> list){ 
 		long start = System.currentTimeMillis();
 		List<Hero> temp = new ArrayList<>();
 		for (Hero hero : list) {
@@ -20,41 +38,18 @@ public class MyHashMapCompare {
 				temp.add(hero);
 			}
 		}
-		System.out.println("使用for循环，遍历出来的结果是：");
-		for (int i = 0; i < temp.size(); i++) {
-			Hero hero = temp.get(i);
-			System.out.printf(hero.name + " ");
-			if (i % 5 == 4) {
-				System.out.println();
-			}
-		}
 		long end = System.currentTimeMillis();
-		System.out.printf("for循环查找耗费%d毫秒的时间", end - start);
-		MyHashMap map = new MyHashMap();
-		List<Hero> values;
-		for (Hero hero : list) {
-			if (map.get(hero.name) != null) {
-				List<Hero> value = (List<Hero>) map.get(hero.name);
-				value.add(hero);
-				continue;
-			}
-			values = new ArrayList();
-			values.add(hero);
-			map.put(hero.name, values);
-		}
-		// 使用MyHashMap
-		start = System.currentTimeMillis();
+		System.out.printf("使用for循环，查询得到%d个hero对象,耗费%d毫秒%n",temp.size(),end-start);
+		return temp;
+	}
+	
+	private static List<Hero> findByMyHashMap(MyHashMap map){
+		long start = System.currentTimeMillis();
 		System.out.println("使用MyHashMap: ");
-		List<Hero> temp2 = (List<Hero>) map.get("hero-5555");
-		for (int i = 0; i < temp2.size(); i++) {
-			Hero hero = temp2.get(i);
-			System.out.printf(hero.name + " ");
-			if (i % 5 == 4) {
-				System.out.println();
-			}
-		}
-		end = System.currentTimeMillis();
-		System.out.printf("MyHashMap查询耗费%d毫秒", end - start);
+		List<Hero> temp = (List<Hero>) map.get("hero-5555");
+		long end = System.currentTimeMillis();
+		System.out.printf("MyHashMap查询得到%d个hero对象，耗费%d毫秒",temp.size(), end - start);
+		return temp;
 	}
 
 }
