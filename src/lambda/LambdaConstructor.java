15b3844bca9000f9aa4bede76c9a1301756c23fc
ZhaiDongboBug <18747867831@163.com>
2020-05-28 15:57:16 +0800

    lambda引用构造器-2
    总结：
    1.需要调用的构造器的参数列表必须与函数式接口中抽象方法的参数列表保持一致。
    https://blog.csdn.net/PlainWaterh/article/details/84107951

--- src/lambda/LambdaConstructor.java
+++ src/lambda/LambdaConstructor.java
@@ -18,16 +18,12 @@ public class LambdaConstructor {
 	}
 
 	private static void insertFirst(Supplier<List> l, String type) {
-		if (l instanceof ArrayList) {
-			ArrayList st = (ArrayList) l;
-		} else if (l instanceof LinkedList) {
-			LinkedList st = (LinkedList) l;
-		}
 		int total = 1000 * 100;
 		final int number = 5;
 		long start = System.currentTimeMillis();
+		List list = l.get();
 		for (int i = 0; i < total; i++) {
-			st.add(0, number);
+			list.add(0, number);
 		}
 		long end = System.currentTimeMillis();
 		System.out.printf("在%s 最前面插入%d条数据，总共耗时 %d 毫秒 %n", type, total, end - start);
