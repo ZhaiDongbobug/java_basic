47839690a3c0575361a33bdc5056a346abf26d28
ZhaiDongboBug <18747867831@163.com>
2020-05-29 13:48:05 +0800

    聚合操作-2
    总结：
    1.为什么输出集合的toString会换行？
    超过控制台的最长字符数就会换行
    2.一个方法的参数类型必须是函数式接口，才可以
    用Lambda,并且这个Lambda必须是实现该类型接口的方法

--- src/lambda/LambdaAggregate.java
+++ src/lambda/LambdaAggregate.java
@@ -3,31 +3,33 @@ package lambda;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 
 public class LambdaAggregate {
 
 	public static void main(String[] args) {
-		int length = 10;
-		List<Hero> heros = new ArrayList<>(length);
-		for (int i = 0; i < length; i++) {
-			Hero hero = new Hero("hero-" + i);
-			hero.damage = (int) (Math.random() * 100 + 50);
-			hero.hp = (int) (Math.random() * 1000 + 500);
-			heros.add(hero);
+		Random r = new Random();
+		List<Hero> heros = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			heros.add(new Hero("hero-" + i, r.nextInt(1000), r.nextInt(100)));
 		}
 		System.out.println("用传统方式打印hp第三高的英雄名称");
 		// 在Hero类中，compareHero方法以hp的从小到大排列
-		List<Hero> heros1 = new ArrayList<>(Arrays.asList(new Hero[heros.size()]));
-		Collections.copy(heros1, heros);
-		heros1.sort(Hero::compareHero);
+
+		Collections.sort(heros, new Comparator<Hero>() {
+			public int compare(Hero h1, Hero h2) {
+				return (int) (h2.hp - h1.hp);
+			}
+		});
+		System.out.println(heros.toString());
 		System.out.println("--------------");
-		System.out.println("第三高的英雄：" + heros1.get(length - 4).name);
+		System.out.println("第三高的英雄：" + heros.get(2).name);
 		System.out.println("用聚合操作打印hp第三高的英雄名称");
-		List<Hero> heros2 = new ArrayList<>(Arrays.asList(new Hero[heros.size()]));
-		Collections.copy(heros2, heros);
 		System.out.println("--------------");
-		Hero hero = heros2.stream().sorted(Hero::compareHero).filter(predicate);
-		System.out.println(hero.name);
+		String name = heros.stream().sorted((h1, h2) -> h2.hp > h1.hp ? 1 : -1).skip(2).map(h -> h.name).findFirst()
+				.get();
+		System.out.println("第三高的英雄：" + name);
 	}
 }
