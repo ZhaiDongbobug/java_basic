f96697772acc0344e03c9103a106450af2ac7a2c
ZhaiDongboBug <18747867831@163.com>
2020-06-01 11:21:23 +0800

    线程交互-1
    总结：
    1.关于this.wait()和this.notify()的理解
    wait是离开教室，notify是告诉下一个人进入教室

--- src/lambda/Hero.java
+++ src/lambda/Hero.java
@@ -3,33 +3,66 @@ package lambda;
 public class Hero {
 
 	public String name;
-    public float hp;
-  
-    public int damage;
-  
-    public Hero() {
-    	this.name = null;
-    }
-  
-    public Hero(String name) {
- 
-        this.name = name;
-    }
-  
-    public String toString() {
-        return "Hero [name=" + name + ", hp=" + hp + ", damage=" + damage + "]\r\n";
-    }
- 
-    public Hero(String name, int hp, int damage) {
-        this.name = name;
-        this.hp = hp;
-        this.damage = damage;
-    }
-    public int compareHero(Hero h2) {
+	public float hp;
+
+	public int damage;
+
+	public Hero() {
+		this.name = null;
+	}
+
+	public Hero(String name) {
+
+		this.name = name;
+	}
+
+	public String toString() {
+		return "Hero [name=" + name + ", hp=" + hp + ", damage=" + damage + "]\r\n";
+	}
+
+	public Hero(String name, int hp, int damage) {
+		this.name = name;
+		this.hp = hp;
+		this.damage = damage;
+	}
+
+	public int compareHero(Hero h2) {
 		// 按照hp进行排序
 		if (this.hp >= h2.hp)
 			return 1; // 正数表示h1比h2要大
 		else
 			return -1;
 	}
+
+	//
+	public synchronized void recover() {
+		if (hp == 1000) {
+			try {
+				// 让占有this的减血线程，暂时释放对this的占有，并等待
+				this.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		hp = hp + 1;
+		System.out.printf("%s 回血1点,增加血后，%s的血量是%.0f%n", name, name, hp);
+		this.notify();
+	}
+
+	public synchronized void hurt() {
+		if (hp == 1) {
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		hp = hp - 1;
+		System.out.printf("%s 减血1点,减少血后，%s的血量是%.0f%n", name, name, hp);
+		// 通知那些等待在this对象上的线程，可以醒过来了
+		this.notify();
+	}
+
 }
--- src/multithreading/ThreadTest.java
+++ src/multithreading/ThreadTest.java
@@ -1,83 +1,43 @@
 package multithreading;
 
-
 import lambda.Hero;
 
 public class ThreadTest {
 
 	public static void main(String[] args) {
-		final Hero a = new Hero();
-		final Hero b = new Hero();
-		final Hero c = new Hero();
+		final Hero gareen = new Hero();
+		// final Hero b = new Hero();
+		// final Hero c = new Hero();
 
 		Thread t1 = new Thread() {
 			public void run() {
-				// 占有对象a
-				synchronized (a) {
-					System.out.println("t1 已占有九尾妖狐");
+				while (true) {
+					gareen.hurt();
+
 					try {
-						// 停顿1000毫秒，另一个线程有足够的时间占有安妮
-						Thread.sleep(1000);
+						Thread.sleep(100);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-
-					System.out.println("t1 试图占有对象b");
-					System.out.println("t1 等待中 。。。。");
-					synchronized (b) {
-						System.out.println("do something");
-					}
 				}
-
 			}
 		};
 		t1.start();
 		Thread t2 = new Thread() {
 			public void run() {
-				// 占有对象b
-				synchronized (b) {
-					System.out.println("t2 已占有对象b");
-					try {
+				while (true) {
+					gareen.recover();
 
-						// 停顿1000毫秒，另一个线程有足够的时间占有暂用九尾妖狐
-						Thread.sleep(1000);
+					try {
+						Thread.sleep(10);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-					System.out.println("t2 试图占有对象c");
-					System.out.println("t2 等待中 。。。。");
-					synchronized (c) {
-						System.out.println("do something");
-					}
 				}
-
 			}
 		};
 		t2.start();
-		Thread t3 = new Thread() {
-			public void run() {
-				// 占有对象b
-				synchronized (c) {
-					System.out.println("t2 已占有对象c");
-					try {
-
-						// 停顿1000毫秒，另一个线程有足够的时间占有暂用九尾妖狐
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					System.out.println("t2 试图占有对象a");
-					System.out.println("t2 等待中 。。。。");
-					synchronized (a) {
-						System.out.println("do something");
-					}
-				}
-
-			}
-		};
-		t3.start();
 	}
 }
