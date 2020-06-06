2fa8d37c46012875f29ad91a9ea55b7d53e7c17b
ZhaiDongboBug <18747867831@163.com>
2020-06-06 11:40:39 +0800

    jdbc增删改

--- src/jdbc/TestJDBC.java
+++ src/jdbc/TestJDBC.java
@@ -8,7 +8,10 @@ import java.sql.Statement;
 public class TestJDBC {
 
 	public static void main(String[] args) {
+		execute("delete from hero where id = 1");
+	}
 
+	public static void execute(String sql) {
 		try {
 			// Class.forName是把这个类加载到JVM中，
 			// 加载的时候，就会执行其中的静态初始化块，
@@ -26,11 +29,9 @@ public class TestJDBC {
 						.getConnection("jdbc:mysql://127.0.0.1:3306/how2java?characterEncoding=UTF-8", "root", "admin");
 				// Statement是用于执行SQL语句的，比如增加，删除
 				Statement s = c.createStatement();) {
-			for (int i = 0; i < 100; i++) {
-				// 原来在数据库中给字段动态赋值需要以‘“+···+”’的方式来完成
-				String sql = "insert into hero values(null," + "'英雄-" + i + "'" + "," + 313.0f + "," + 50 + ")";
-				s.execute(sql);
-			}
+
+			s.execute(sql);
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
