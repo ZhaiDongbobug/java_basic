932bcac05d9e629f11a5d9543ca45945a315dcba
ZhaiDongboBug <18747867831@163.com>
2020-06-10 18:11:04 +0800

    网络编程之ping-2
    总结：
    1.ping -n 1是什么意思
    答：意思是只向目标IP试探一次，如果不加这条，默认会试探3次
    2.为什么内部类调用的外部变量必须是final修饰的？
    https://blog.csdn.net/u010393325/article/details/80643636
    3.为什么要关闭线程池？
    不关闭你怎么出execute()方法
    4.主机开着防火墙，所以ping不通

--- src/networkProgramming/TestSocket.java
+++ src/networkProgramming/TestSocket.java
@@ -11,61 +11,62 @@ import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TestSocket {
 
-	public static void main(String[] args) throws UnknownHostException {
+	public static void main(String[] args) throws UnknownHostException, InterruptedException {
 		ThreadPoolExecutor threadPool = new ThreadPoolExecutor(10, 20, 60, TimeUnit.SECONDS,
 				new LinkedBlockingQueue<Runnable>());
 		InetAddress host = InetAddress.getLocalHost();
 		String ip = host.getHostAddress();
 		System.out.println(ip);
-		String[] strArry = ip.split("\\.");
-		System.out.println(strArry.length);
-		String str = "";
-		for (int i = 0; i < 3; i++) {
-			str = str + strArry[i] + ".";
-		}
-		List<String> list = (List<String>) Collections.synchronizedList(new ArrayList<String>());
+		String ipRange = ip.substring(0, ip.lastIndexOf("."));
+		System.out.println("网段是:" + ipRange);
+		List<String> list = Collections.synchronizedList(new ArrayList<>());
+		AtomicInteger number = new AtomicInteger();
 		for (int i = 1; i < 256; i++) {
-			String strIp = str + i;
+			String strIp = ipRange + "." + i;
 			threadPool.execute(new Runnable() {
 				public void run() {
-					Process p;
-					try {
-						p = Runtime.getRuntime().exec("ping " + strIp);
-						BufferedReader br;
-						try {
-							br = new BufferedReader(new InputStreamReader(p.getInputStream(), "gbk"));
-							String line = null;
-							StringBuilder sb = new StringBuilder();
-							while ((line = br.readLine()) != null) {
-								if (line.length() != 0)
-									sb.append(line + "\r\n");
-							}
-							String print = sb.toString();
-							System.out.println(print);
-							String error = "请求超时。";
-
-							if (!print.contains(error)) {
-								list.add(strIp);
-							}
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+					boolean reachable = isReachable(strIp);
+					if (reachable) {
+						list.add(strIp);
+					}
+					synchronized (number) {
+						System.out.println("已经完成：" + number.incrementAndGet() + "个ip测试");
 					}
 
 				}
 			});
-			System.out.println("已经完成：" + i + "个ip测试");
 		}
-		System.out.println("如下ip地址可以连接");
-		for (int i = 0; i < list.size(); i++) {
-			System.out.println(list.get(i));
+		threadPool.shutdown();
+		if (threadPool.awaitTermination(1, TimeUnit.HOURS)) {
+			System.out.println("如下ip地址可以连接");
+			for (int i = 0; i < list.size(); i++) {
+				System.out.println(list.get(i));
+			}
 		}
+
+	}
+
+	private static boolean isReachable(String strIp) {
+		boolean reachable = false;
+		try {
+			Process p = Runtime.getRuntime().exec("ping -n 1 " + strIp);
+			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream(), "GBK"));
+			String line = null;
+			StringBuilder sb = new StringBuilder();
+			while ((line = br.readLine()) != null) {
+				if (line.length() != 0)
+					sb.append(line + "\r\n");
+			}
+			// System.out.println(sb.toString());
+			reachable = sb.toString().contains("TTL");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return reachable;
 	}
 }
