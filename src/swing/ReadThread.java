ef0a98b840e6dd155122554a93f9df8d52863468
ZhaiDongboBug <18747867831@163.com>
2020-06-13 09:48:39 +0800

    图形界面之swing-2

--- src/swing/ReadThread.java
+++ src/swing/ReadThread.java
@@ -7,30 +7,30 @@ import java.io.IOException;
 
 import javax.swing.JFrame;
 
-public class ReadThread extends Thread{
+public class ReadThread extends Thread {
 
 	JFrame j;
+
 	public ReadThread(JFrame j) {
 		this.j = j;
 	}
+
 	public void run() {
-		while(true) {
+		while (true) {
+			File f = new File("d:/location.txt");
+			try (FileOutputStream fos = new FileOutputStream(f); DataOutputStream dos = new DataOutputStream(fos);) {
+				dos.writeInt(j.getX());
+				dos.writeInt(j.getY());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			File f =new File("d:/location.txt");
-	        try (
-	                FileOutputStream fos  = new FileOutputStream(f);
-	                DataOutputStream dos =new DataOutputStream(fos);
-	        ){
-	            dos.writeInt(j.getX());
-	            dos.writeInt(j.getY());
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-		}	
+		}
 	}
 }
--- src/swing/TestGUI.java
+++ src/swing/TestGUI.java
@@ -7,34 +7,51 @@ import java.io.FileInputStream;
 
 import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class TestGUI {
 
 	public static void main(String[] args) {
-		int X = 200;
-		int Y = 200;
 		JFrame j = new JFrame("LoL");
-		read(j,X, Y);
+		j.setSize(400, 300);
+		Point p = read();
+		if (p != null) {
+			j.setLocation(p.x, p.y);
+		} else {
+			j.setLocation(200, 200);
+		}
+		j.setLayout(null);
+		JButton b = new JButton("一键秒对方基地挂");
+
+		// 同时设置组件的大小和位置
+		b.setBounds(50, 50, 280, 30);
+
+		// 把按钮加入到主窗体中
+		j.add(b);
+
+		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		j.setVisible(true);
 		new ReadThread(j).start();
 	}
 
-	private static void read(JFrame j,int X, int Y) {
+	static class Point {
+		int x;
+		int y;
+	}
+
+	private static Point read() {
 		File f = new File("d:/location.txt");
+		Point p = null;
 		try (FileInputStream fis = new FileInputStream(f); DataInputStream dis = new DataInputStream(fis);) {
-			if(dis.read() != -1) {
-				X = dis.readInt();
-				Y = dis.readInt();
-			}		
-			j.setSize(400, 300);
-			j.setLocation(X, Y);
-			j.setLayout(null);
-			j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        // 让窗体变得可见
-	        j.setVisible(true);
+			int x = dis.readInt();
+			int y = dis.readInt();
+			p = new Point();
+			p.x = x;
+			p.y = y;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		return p;
 	}
 }
